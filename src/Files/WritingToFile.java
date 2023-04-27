7e843661b60f3cbc4d74f9530a4642b7a81c1b5d
MaksMusic <Maksmusic05@yandex.ru>
2023-04-27 17:53:06 +0300

    write to File

--- src/Files/WritingToFile.java
+++ src/Files/WritingToFile.java
@@ -1,6 +1,5 @@
 package Files;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,13 +12,13 @@ public class WritingToFile {
     }
 
     /**
-     *Files.write() - это стандартный метод Java для записи в файл, он записывает байты в файл через OutputStream.
+     * Files.write() - это стандартный метод Java для записи в файл, он записывает байты в файл через OutputStream.
      * Он позволяет добавлять данные в уже существующий файл, используя параметр
      * java.nio.file.StandardOpenOption.APPEND.
      * или StandardOpenOption.WRITE для перезаписывания данных в файл
      * Также он автоматически закрывает соединение с файлом после записи.
      */
-    public void write(Path path,String text){
+    public void write(Path path, String text) {
         try {
             Files.write(path, text.getBytes(), Files.exists(path) ? java.nio.file.StandardOpenOption.APPEND :
                     java.nio.file.StandardOpenOption.CREATE);
@@ -30,16 +29,16 @@ public class WritingToFile {
     }
 
     /**
-     FileWriter - класс из библиотеки java.io, который также используется для записи данных в файл.
-     Он более удобен для записи текстовых строк в файл,
-     так как он позволяет записывать строки без преобразования в байты,
-     а также добавлять символы к уже существующим строкам
+     * FileWriter - класс из библиотеки java.io, который также используется для записи данных в файл.
+     * Он более удобен для записи текстовых строк в файл,
+     * так как он позволяет записывать строки без преобразования в байты,
+     * а также добавлять символы к уже существующим строкам
      */
-    public void write2(String text1){
-        try{
+    public void write2(String text1) {
+        try {
             // Устанавливается значение true, что означает, что новые данные будут дописываться в конец файла.
             // Если бы значение было false, то файл бы перезаписывался заново при выполнении записи
-            FileWriter writer = new FileWriter("listUser.txt",false);    //создать новый файл
+            FileWriter writer = new FileWriter("listUser.txt", false);    //создать новый файл
             writer.write(text1);    //добавить строку
             writer.flush();         // добавить все изменения в файл
 
@@ -47,14 +46,10 @@ public class WritingToFile {
             writer.flush();         // добавить все изменения в файл
             writer.close();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-
-
-
-
 
 
 }
