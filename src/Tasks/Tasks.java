0bf549e66d7046499d42d9f7f2b8994910aef7ff
MaksMusic <Maksmusic05@yandex.ru>
2023-05-01 16:24:03 +0300

    write to File

--- src/Tasks/Tasks.java
+++ src/Tasks/Tasks.java
@@ -1,4 +1,4 @@
-package Task;
+package Tasks;
 
 import java.io.File;
 import java.io.IOException;
--- src/Tasks/projectComments/Comment.java
+++ src/Tasks/projectComments/Comment.java
@@ -1,4 +1,4 @@
-package projectComments;
+package Tasks.projectComments;
 
 import java.io.IOException;
 import java.nio.file.Files;
