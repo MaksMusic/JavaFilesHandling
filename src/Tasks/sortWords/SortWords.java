4d17c660c3ba35ee687d40b82792d7a0d8fbdc44
MaksMusic <Maksmusic05@yandex.ru>
2023-05-16 14:22:09 +0300

    SortWords

---
+++ src/Tasks/sortWords/SortWords.java
@@ -0,0 +1,66 @@
+package Tasks.sortWords;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * принимает файл текстового документа, сортирует все слова на буквы и записывает их в соответствующие файлы:
+ * напрмер слова на а в файл а.txt по указанному пути
+ */
+
+public class SortWords {
+    public static void main(String[] args) throws IOException {
+        // Открываем файл для чтения
+        BufferedReader reader = new BufferedReader(new FileReader("d:\\Users\\Admin\\Desktop\\re.txt"));
+        // Создаем хранилище для слов и файлов по первой букве слова
+        Map<Character, List<String>> wordMap = new HashMap<>();
+        // Читаем файл построчно
+        String line;
+        while ((line = reader.readLine()) != null) {
+            // Разбиваем строку на слова и обрабатываем каждое слово
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                // Пропускаем слова, начинающиеся не с буквы
+                if (!Character.isLetter(word.charAt(0))) {
+                    continue;
+                }
+                // Получаем первую букву слова, переводим ее в нижний регистр
+                char firstChar = Character.toLowerCase(word.charAt(0));
+                // Получаем список слов, начинающихся на эту букву, или создаем новый список
+                List<String> wordList = wordMap.getOrDefault(firstChar, new ArrayList<>());
+                // Добавляем слово в список
+                wordList.add(word);
+                // Сохраняем список слов для этой буквы
+                wordMap.put(firstChar, wordList);
+            }
+        }
+        // Закрываем файл для чтения
+        reader.close();
+
+
+        // Создаем файлы для записи слов
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            String filename = "C:\\words\\"+ letter + ".txt";
+
+            // Указываем путь до папки, в которую будут записаны файлы
+            String folderPath = "C:\\words";
+            // Создаем объект File для указанной папки
+            File folder = new File(folderPath);
+            // Если папки не существует, то создаем ее
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            // Получаем список слов, начинающихся на эту букву
+            List<String> wordList = wordMap.getOrDefault(letter, new ArrayList<>());
+            // Сортируем список слов и записываем его в файл
+            Collections.sort(wordList);
+            for (String word : wordList) {
+                writer.write(word + "\n");
+            }
+            // Закрываем файл для записи
+            writer.close();
+        }
+    }
+}
